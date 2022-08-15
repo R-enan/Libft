55b9e48a47872ea56ceed241ad3c252f60e611a4
R-enan <silva.renanleite@gmail.com>
2022-08-15 19:01:28 +0200

    Atualização de lógica

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,24 +6,22 @@
 /*   By: rleite-s <rleite-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 23:09:46 by rleite-s          #+#    #+#             */
-/*   Updated: 2022/06/11 23:01:18 by rleite-s         ###   ########.fr       */
+/*   Updated: 2022/07/20 01:24:32 by rleite-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_abs(int n)
-{
-	if (n < 0)
-		return (-n);
-	return (n);
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
+	{
 		write(fd, "-", 1);
-	if (n < -9 || n > 9)
-		ft_putnbr_fd(ft_abs(n / 10), fd);
-	write(fd, &"0123456789"[ft_abs(n % 10)], 1);
+		if (n < -9)
+			ft_putnbr_fd(-(n / 10), fd);
+		n = -(n % 10);
+	}
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	write(fd, &"0123456789"[n % 10], 1);
 }
